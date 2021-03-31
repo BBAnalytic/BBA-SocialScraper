60b2b9b5b6d5e0e1af7f11356433d3efa0f4cecd
abdul_karim <abdullah.karim@maine.edu>
2021-03-31 16:11:06 -0400

    Changes to the api for refactoring names

--- API/api.py
+++ API/api.py
@@ -1,6 +1,6 @@
 """
 Created by: Abdul Karim
-Created on: Feb 2021
+Created on: 03/31/21
 Version: 1.0
 Description: File contains the database schema for the user database as well as endpoints that can be used to access backend processes and the database.
 """
@@ -40,10 +40,10 @@ class UserDB(o_db.Model):
       return f'{self.id} {self.content}'
 
 @m_app.route('/api/loginUser/', methods = ['POST'])
-def json_loginUser():
+def _json_login_user():
    """
    Description: Allows a frontend process to validate if user credentials are correct.
-   Arguements: Non, but json body requested needs to look like this:
+   Arguements: None, but json body requested needs to look like this:
                {
                   "email": "email@email.com",
                   "password": "password"
@@ -74,13 +74,11 @@ def json_loginUser():
    else:
       return jsonify({'result': 'NOK User Not Found'})
 
-# User creation is happening. Check to see if the email is already in use and if not,
-# Create a new user entry.
 @m_app.route('/api/createUser/', methods = ['POST'])
-def createUser():   
+def _json_create_user():   
    """
    Description: Allows a frontend process to create a user and store that user in the database.
-   Arguements: Non, but json body requested needs to look like this:
+   Arguements: None, but json body requested needs to look like this:
                {
                   "email": "email@email.com",
                   "name": "First Last",
@@ -140,63 +138,79 @@ def createUser():
    else:
       return jsonify({'result': 'NOK No Email Input'})
 
-#DONE
 @m_app.route('/api/deleteUser/', methods=['POST'])
-def delete():
-   # Get the user information. JSON Body: {"email": "email@email.com"}
+def _json_delete_user():
+   """
+   Description: Allows a frontend process to access the database and delete a user.
+   Arguements: None, but json body requested needs to look like this:
+               {
+                  "email": "email@email.com",
+               }
+   Outputs: JSON body signaling whether or not the information has been validated.    
+            Looks like this:
+            {
+               "result": "OK/NOK followed by a message."
+            }
+   """
+   # Grab inputs
    json_request_data = json.loads(request.data)
    s_inputEmail = json_request_data['email'] 
    
    # Check to see if the user exists
-   # exists = o_db.session.query(UserDB.email).filter_by(email=inputEmail).first()
    if(o_db.session.query(UserDB.s_email).filter_by(s_email = s_inputEmail).delete()):
       o_db.session.commit()
       return jsonify({'result': 'OK User deleted'})
    else:
       return jsonify({'result': 'NOK User does not exist'})
 
-# This needs to go
-# # Edit a user DO AFTER PRESENTATION DEADLINE
-# @m_app.route('/api/editUser/', methods=['POST'])
-# def edit():
-#    pass
-
-################################################################################
-#
-#
-# INSTAGRAM SCRAPING ENDPOINTS
-#
-#
-################################################################################
-# DONE
 @m_app.route('/api/scrapeInstagram/', methods =['POST'])
-def scrapeInstagram():
-   # Get the user information. JSON body: {"search_term": "hashtag/person/location", "search_category": "hashtag or location"}
+def _json_scrape_instagram():
+   """
+   Description: Allows a frontend process to process a request to scrape instagram, given inputs.
+   Arguements: None, but json body requested needs to look like this:
+               {
+                  "search_term": "#hashtag#stuff OR locationurl",
+                  "search_category": "the word: hashtag or the word: location"
+               }
+   Outputs: JSON body signaling whether or not the information has been validated.    
+            Looks like this:
+            {
+               "result": "OK/NOK followed by a message."
+            }
+   """
+   # Grab inputs
    json_request_data = json.loads(request.data)
 
    s_search_term = json_request_data['search_term']
    s_search_category = json_request_data['search_category']
 
+   # Call the function that scrapes instagram.
    v_url_extractor(s_search = s_search_term, s_category = s_search_category)
    v_read_to_queue()
-   return jsonify({'result': 'Instagram Query Complete'})
 
-################################################################################
-#
-#
-# TWITTER SCRAPING ENDPOINTS
-#
-#
-################################################################################
+   return jsonify({'result': 'OK Instagram Query Complete'})
 
-# Make sure this isn't set to have GET with it as well.
-# Done
 @m_app.route('/api/scrapeTwitter/', methods=['POST'])
-def scrapeTwitter():
-   # Get the user information. All lists are comma-serparated. JSON body: {"#hashTags": "#list#of#tags", "locations": "#list#of#locations", "phrases": "#list#of#phrases", "earliestDate": "yyyyMMddHHmm", "latestDate": "yyyyMMddHHmm"}
+def _json_scrape_twitter():
+   """
+   Description: Allows a frontend process to process a request to scrape instagram, given inputs.
+   Arguements: None, but json body requested needs to look like this:
+               {
+                  "#hashTags": "#list#of#tags",
+                  "locations": "#list#of#locations",
+                  "phrases": "#list#of#phrases",
+                  "earliestDate": "yyyyMMddHHmm",
+                  "latestDate": "yyyyMMddHHmm"
+               }
+   Outputs: JSON body signaling whether or not the information has been validated.    
+            Looks like this:
+            {
+               "result": "OK/NOK followed by a message."
+            }
+   """
+   # Grab inputs
    json_request_data = json.loads(request.data)
 
-   # Fix this so that we don't have .split(",")
    l_hashTags = json_request_data['hashTags'].split("#")
    l_hashTags.pop(0)
    l_locations = json_request_data['locations'].split("#")
@@ -218,48 +232,12 @@ def scrapeTwitter():
    elif (len(l_phrases) <= 0):
       l_phrases = None
 
+   # Run a twitter scrape
    s_query = s_build_query(l_hashTags, l_locations, l_phrases)
    v_scrape_tweets(s_query = s_query, s_earliest = b_earliest_date, s_latest = b_latest_date)
 
-   return jsonify({'result': 'Twitter Query Complete'})
+   return jsonify({'result': 'OK Twitter Query Complete'})
 
-# todo: Must return what date range the uses can select from so the calendar can be generated with that information.
-# todo: m_app.route to send email
-# @m_app.route('/api/getTwitterDateRange', methods=['GET'])
-# def getEarlyLateRange():
-#    pass
-
-################################################################################
-#
-#
-# SCAFFOLDING
-#
-#
-################################################################################
+# Starts the application when this function is started.
 if __name__ == '__main__':
    m_app.run(debug = True)
-
-# Initialize the DB Model with a user example.
-# python3
-# from api import o_db
-# o_db.create_all ()
-
-# from api import UserDB
-# user = UserDB(email = "a2a.a", password = "foo", first = "bar", last = "pie", admin = True, approved = True)
-# o_db.session.add(user)
-# o_db.session.commit()
-
-# ------------------------------------------------------------------------------
-
-# create TABLE users (
-#     id INTEGER not null,
-#     can_view_records BOOLEAN not null,
-#     PRIMARY KEY (id)
-# );
-
-# #someone goes to /api/ any id, pass back that id
-# @m_app.route('/api/<int:id>')
-# def show(id):
-#    print ("This is the id: " + str(id))
-#    return jsonify([*map(user_serializer, UserDB.query.filter_by(id=id))])
-
